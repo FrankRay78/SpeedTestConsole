350b2cc6085677e49eb76ee8f1fcab9c5388b4d9
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2025-01-25 21:08:08 +0000

    Help includes application description at root.

--- src/SpeedTestConsole/CustomHelpProvider.cs
+++ src/SpeedTestConsole/CustomHelpProvider.cs
@@ -3,6 +3,32 @@ using Spectre.Console.Rendering;
 
 internal class CustomHelpProvider : HelpProvider
 {
+    /// <summary>
+    /// Used to provide a description for the application root.
+    /// </summary>
+    private class DummyCommand : ICommandInfo
+    {
+        public string Name => throw new NotImplementedException();
+
+        public string? Description { get; set; }
+
+        public bool IsBranch => throw new NotImplementedException();
+
+        public bool IsDefaultCommand => throw new NotImplementedException();
+
+        public bool IsHidden => throw new NotImplementedException();
+
+        public IReadOnlyList<ICommandParameter> Parameters => throw new NotImplementedException();
+
+        public ICommandInfo? Parent => throw new NotImplementedException();
+
+        public IReadOnlyList<string[]> Examples => throw new NotImplementedException();
+
+        public IReadOnlyList<ICommandInfo> Commands => throw new NotImplementedException();
+
+        public ICommandInfo? DefaultCommand => throw new NotImplementedException();
+    }
+
     public CustomHelpProvider(ICommandAppSettings settings)
         : base(settings)
     {
@@ -15,10 +41,20 @@ internal class CustomHelpProvider : HelpProvider
         return
         [
             Text.NewLine,
-            new FigletText(font, "Speedtest Console")
+            new FigletText(font, "SpeedTestConsole")
                 .LeftJustified()
                 .Color(Color.Gold1),
             Text.NewLine
         ];
+    }
+
+    public override IEnumerable<IRenderable> GetDescription(ICommandModel model, ICommandInfo? command)
+    {
+        if (command is null)
+        {
+            command = new DummyCommand { Description = "Internet speed tester including server discovery, latency measurement, and download speed testing." };
+        }
+
+        return base.GetDescription(model, command);
     }
 }
