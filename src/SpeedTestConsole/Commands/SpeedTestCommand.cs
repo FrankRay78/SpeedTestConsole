bfbd0fc59fcfe3395fdb58df240b806881ab4a85
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2025-03-01 18:12:08 +0000

    New SpeedTestCommand

---
+++ src/SpeedTestConsole/Commands/SpeedTestCommand.cs
@@ -0,0 +1,88 @@
+﻿using ByteSizeLib;
+using Humanizer;
+
+namespace SpeedTestConsole.Commands;
+
+public sealed class SpeedTestCommand : AsyncCommand<SpeedTestCommandSettings>
+{
+    private IAnsiConsole console;
+    private ISpeedTestService speedTestClient;
+    private IClock clock;
+
+    public SpeedTestCommand(IAnsiConsole console, ISpeedTestService speedTestClient, IClock clock)
+    {
+        this.console = console;
+        this.speedTestClient = speedTestClient;
+        this.clock = clock;
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, SpeedTestCommandSettings settings)
+    {
+        var servers = await speedTestClient.GetServersAsync();
+
+
+        var fastest = await speedTestClient.GetFastestServerByLatencyAsync(servers);
+
+        if (fastest == null)
+        {
+            throw new Exception("No servers available");
+        }
+
+        if ((settings.Verbosity & (Verbosity.Normal | Verbosity.Debug)) != 0)
+        {
+            console.WriteLine($"{fastest.Value.server.Sponsor} ({fastest.Value.latency} ms)");
+        }
+
+
+        SpeedTestResult result = new SpeedTestResult();
+
+        if ((settings.Verbosity & Verbosity.Minimal) != 0)
+        {
+            result = await speedTestClient.GetDownloadSpeedAsync(fastest.Value.server);
+        }
+        else
+        {
+            // Graphical progress bar
+            await console.Progress()
+                .AutoClear(false)
+                .Columns(
+                [
+                    new TaskDescriptionColumn(),
+                    new ProgressBarColumn(),
+                    new PercentageColumn(),
+                ])
+                .StartAsync(async progress =>
+                {
+                    var downloadProgressTask = progress.AddTask("Downloading", autoStart: true, maxValue: 100);
+
+                    Action<int> UpdateProgress = (int percentageComplete) =>
+                    {
+                        // Update the progress bar
+                        downloadProgressTask.Value = percentageComplete;
+                    };
+
+                    result = await speedTestClient.GetDownloadSpeedAsync(fastest.Value.server, UpdateProgress);
+                });
+        }
+
+
+        var size = ByteSize.FromBytes(result.BytesProcessed);
+        var elapsed = TimeSpan.FromMilliseconds(result.ElapsedMilliseconds);
+
+        if ((settings.Verbosity & Verbosity.Debug) != 0)
+        {
+            console.WriteLine($"{size.ToString()} downloaded in {elapsed.Humanize()}");
+        }
+
+        var speedString = result.GetSpeedString(settings.SpeedUnit);
+
+        if (settings.IncludeTimestamp)
+        {
+            console.Write($"{clock.Now.ToString(settings.DateTimeFormat)} ");
+        }
+        console.WriteLine($"Download: {speedString}");
+
+
+        return 0;
+    }
+}
---
+++ src/SpeedTestConsole/Commands/SpeedTestCommandSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace SpeedTestConsole.Commands;
+
+public sealed class SpeedTestCommandSettings : CommandSettings
+{
+    [CommandOption("-t | --timestamp")]
+    [Description("Include a timestamp.")]
+    [DefaultValue(false)]
+    public bool IncludeTimestamp { get; set; }
+
+    [CommandOption("--datetimeformat", IsHidden = true)]
+    [Description("The date and time format string (as defined by Microsoft.Net).")]
+    [DefaultValue("yyyy'-'MM'-'dd' 'HH':'mm':'ss")]
+    //ref: https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings
+    public string? DateTimeFormat { get; set; }
+
+    [CommandOption("-u | --unit")]
+    [Description("The speed unit.\n   BitsPerSecond, BytesPerSecond")]
+    [DefaultValue(SpeedUnit.BitsPerSecond)]
+    public SpeedUnit SpeedUnit { get; set; }
+
+    [CommandOption("--verbosity")]
+    [Description("The verbosity level.\n   Minimal, Normal, Debug\nMinimal is ideal for use in batch scripts.")]
+    [DefaultValue(Verbosity.Normal)]
+    public Verbosity Verbosity { get; set; }
+}
